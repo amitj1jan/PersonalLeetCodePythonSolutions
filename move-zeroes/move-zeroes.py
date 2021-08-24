a1c81dbbe270c7d4cc56b0774756e8bc23b94681
Amit Jha <amitjha@umich.edu>
2021-08-24 22:36:34 +0400

    Runtime: 48 ms, faster than 76.95% of Python3 online submissions for Move Zeroes.

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -3,9 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for num in nums:
-            if num ==0:
-                nums.remove(0)
-                nums.append(0)
+        cnt = nums.count(0)        
+        nums[:] = [num for num in nums if num !=0]
+        nums+= [0] * cnt
         
         
