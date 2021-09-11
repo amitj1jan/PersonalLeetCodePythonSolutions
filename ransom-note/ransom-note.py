384ca3af3d2a82cf28a080f4ea324128fc5fb984
Amit Kumar Jha <amitj1jan@gmail.com>
2021-09-12 01:29:08 +0400

    Time: 77 ms (32.40%), Space: 14.4 MB (35.80%) - LeetHub

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -2,9 +2,5 @@ from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         ransom_c = Counter(ransomNote)
-        magazine_c = Counter(magazine)
-        magazine_c.subtract(ransom_c)
-        if magazine_c.most_common()[-1][1] <0:
-            return(False)
-        else:
-            return(True)
+        magazine_c = Counter(magazine)        
+        return(not ransom_c - magazine_c)
