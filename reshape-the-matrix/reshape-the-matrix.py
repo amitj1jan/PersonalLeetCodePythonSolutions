fea92549befcb148543dd89a8829bcda0ad8bc45
Amit Kumar Jha <amitj1jan@gmail.com>
2021-09-12 02:21:37 +0400

    Time: 144 ms (18.91%), Space: 14.9 MB (74.68%) - LeetHub

--- reshape-the-matrix/reshape-the-matrix.py
+++ reshape-the-matrix/reshape-the-matrix.py
@@ -1,3 +1,4 @@
+import itertools
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         cnt = len(mat) * len(mat[0])
@@ -6,16 +7,8 @@ class Solution:
         out = [[0 for num in range(c)] for num in range(r)]
         m = len(mat)
         n = len(mat[0])
-        mat_flat = [0 for num in range(m) for num in range(n)]
-        indx=0
-        for i in range(m):
-            for j in range(n):
-                mat_flat[indx] = mat[i][j]
-                indx += 1
-        indx = 0
+        nums = itertools.chain(*mat)
         for i in range(r):
             for j in range(c):
-                print(i, j, out[i])
-                out[i][j] = mat_flat[indx]
-                indx += 1
+                out[i][j] = next(nums)
         return(out)
