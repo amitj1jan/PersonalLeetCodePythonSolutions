d756184253864025e86d977b290c2be6c2ec8fa9
Amit Kumar Jha <amitj1jan@gmail.com>
2021-09-21 17:49:33 +0400

    Time: 28 ms (84.17%), Space: 13.9 MB (99.88%) - LeetHub

--- first-bad-version/first-bad-version.py
+++ first-bad-version/first-bad-version.py
@@ -9,14 +9,14 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        left =1
+        left = 1
         right = n
         while right > left:
             mid = left + (right-left)//2            
             if isBadVersion(mid):
                 right = mid
             else:
-                left = mid +1
+                left = mid + 1
         return(left)
             
         
