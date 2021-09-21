f53938d61a6e7977edfedd7c3fc6f6b5b70b156e
Amit Kumar Jha <amitj1jan@gmail.com>
2021-09-21 13:27:55 +0400

    Time: 60 ms (78.47%), Space: 15.4 MB (41.49%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -4,8 +4,9 @@ class Solution:
     '''
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = {}
-        for ind,num in enumerate(nums):
-            if (target - num) in hash_map:
-                return([nums.index(target-num), ind])
+        for indx,num in enumerate(nums):
+            if target-num in hash_map:
+                    return([nums.index(target-num), indx])
             else:
-                hash_map[num] = 1            
+                hash_map[num] = indx
+                
